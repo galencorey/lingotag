6480c589e56eb4fb5043ccb745c86e51947699d8
Galen Corey <galen.corey@gmail.com>
2016-12-28 21:06:45 -0500

    changed cdn order

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
+angular.module('starter', ['ionic', 'firebase', 'starter.controllers', 'starter.services', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -26,7 +26,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
   /*$httpProvider.defaults.headers.post['Authorization'] = 'Bearer dAYYVFSe3iCEgLGF5gGPDEajpMH2NU';
   $httpProvider.defaults.headers.post['Content-Type'] = 'multipart/form-data';*/
-        
+
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
@@ -52,7 +52,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       }
     }
   })
-        
+
   .state('tab.tagselect', {
          url: '/tagselect',
          reload: false,
@@ -61,7 +61,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
              templateUrl: 'templates/tab-tagselect.html'
          }
      }
-      
+
    })
 
   .state('tab.languages', {
